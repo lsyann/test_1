b097fe5da0cf02a59edecb61e58a44a80843607c
asdf <jeffisbestone@gmail.com>
2026-07-12 14:07:59 +0200

    asdf

--- Makefile
+++ Makefile
@@ -1,7 +1,15 @@
 all: run
 
 run:
-	python3 -m src [-functions_definition data/input/functions_definition.json] [-input data/input/function_calling_tests.json] [-output data/output/function_calling_results.json] 
+	uv run -m src [-functions_definition data/input/functions_definition.json] [-input data/input/function_calling_tests.json] [-output data/output/function_calling_results.json]
 
-clean:
-	rm -rf src/__pycache__
+install:
+	uv sync
+
+lint:
+	flake8 .
+	mypy . --warn-return-any --warn-unused-ignores --ignore-missing-imports --disallow-untyped-defs --check-untyped-defs
+
+lint-strict:
+	flake8 .
+	mypy . --strict
--- src/__main__.py
+++ src/__main__.py
@@ -1,20 +1,25 @@
-from .llm import Small_LLM_Model
+from .llm_sdk import Small_LLM_Model
 import json
 from .config import config
 
 
 def get_str(llm: Small_LLM_Model, tokens: list[int], lst: list[int]) -> None:
-    idk = llm._encode('"')[0]
+    idk = llm.encode('"')[0]
     lst.append(idk)
     tokens.append(idk)
 
     while True:
-        # print(llm._decode(tokens))
         logits = llm.get_logits_from_input_ids(tokens)
+
+        for token_id in range(len(logits)):
+            token_str = llm.decode([token_id])
+
+            if "(" in token_str:
+                logits[token_id] = float('-inf')
 
         next_id = logits.index(max(logits))
 
-        if '"' in llm._decode([next_id]):
+        if '"' in llm.decode([next_id]):
             lst.append(idk)
             tokens.append(idk)
             return None
@@ -27,23 +32,22 @@ def get_int(llm: Small_LLM_Model, tokens: list[int], lst: list[int]) -> None:
     first = True
 
     while True:
-        # print(llm._decode(tokens))
         logits = llm.get_logits_from_input_ids(tokens)
 
         for token_id, score in enumerate(logits):
-            token_str = llm._decode([token_id])
+            token_str = llm.decode([token_id])
 
             if first:
                 if token_str.isnumeric() is False and token_str not in ("-"):
                     logits[token_id] = float('-inf')
             else:
                 if token_str.isnumeric() is False and token_str not in (
-                        "-", ",", "."):
+                        "-", ","):
                     logits[token_id] = float('-inf')
 
         next_id = logits.index(max(logits))
 
-        if "," in llm._decode([next_id]):
+        if "," in llm.decode([next_id]):
             return None
 
         if first:
@@ -57,17 +61,17 @@ def main(text: str) -> str:
     with open(definition_file, "r") as f:
         definitions = json.load(f)
 
-    functions = [llm._encode(elem["name"]) for elem in definitions]
+    functions = [llm.encode(elem["name"]) for elem in definitions]
 
     text = text.replace('"', "'")
 
-    lst = llm._encode("{" + f'"prompt": "{text}", "name": "')
+    lst = llm.encode("{" + f'"prompt": "{text}", "name": "')
 
-    tokens = llm._encode(f"user request: {
+    tokens = llm.encode(f"user request: {
                        text}, available functions: {
-                       llm._decode(functions)}, function choice: ")
+                       llm.decode(functions)}, function choice: ")
 
-    temp = llm._encode("think about which function fits the user's request:")
+    temp = llm.encode("think about which function fits the user's request:")
     for elem in temp:
         tokens.append(elem)
 
@@ -77,7 +81,7 @@ def main(text: str) -> str:
 
         next_id = logits.index(max(logits))
 
-        if llm._decode([next_id]) == "\n" or index > 40:
+        if llm.decode([next_id]) == "\n" or index > 40:
             break
         tokens.append(next_id)
         index += 1
@@ -103,27 +107,26 @@ def main(text: str) -> str:
         if len(functions) == 1 and len(functions[0]) == index:
             break
 
-    temp = llm._encode('", "parameters": {')
+    temp = llm.encode('", "parameters": {')
     for elem in temp:
         lst.append(elem)
 
     params = [elem for elem in definitions if elem[
-        "name"] == llm._decode(functions[0])][0]
+        "name"] == llm.decode(functions[0])][0]
 
-    temp = llm._encode(f"prompt: {
-                       text}, use function {
-                       params["name"]}, function description: {
-                       params["description"]}, function parameters: ")
-    tokens = []
+    temp = llm.encode(f"\n[System: Extract parameters for {params['name']}]\n")
     for elem in temp:
         tokens.append(elem)
 
     for key in params["parameters"].keys():
-        temp = llm._encode(f"{key} ({params["parameters"][key]["type"]}), ")
+        temp = llm.encode(f"{key} ({params["parameters"][key]["type"]}), ")
         for nb in temp:
             tokens.append(nb)
 
-    temp = llm._encode("think scratchpad:")
+    temp = llm.encode("Let's extract the exact parameter"
+                      "values from the prompt safely.\n"
+                      f"Target function: {params['name']}\n"
+                      "Extracted Values:\n")
     for elem in temp:
         tokens.append(elem)
 
@@ -133,21 +136,20 @@ def main(text: str) -> str:
 
         next_id = logits.index(max(logits))
 
-        if llm._decode([next_id]) == "\n" or index > 30:
+        if index > 30:
             break
         tokens.append(next_id)
         index += 1
-    # print(llm._decode(tokens))
 
     key = list(params["parameters"].keys())
     for i in range(len(key)):
         arg_type = params["parameters"][key[i]]["type"]
 
-        temp = llm._encode(f'value of param "{key[i]}" ({arg_type}): ')
+        temp = llm.encode(f'value of param "{key[i]}" ({arg_type}): ')
         for elem in temp:
             tokens.append(elem)
 
-        temp = llm._encode(f'"{key[i]}": ')
+        temp = llm.encode(f'"{key[i]}": ')
         for elem in temp:
             lst.append(elem)
 
@@ -159,24 +161,23 @@ def main(text: str) -> str:
             return f"unknown data type: {arg_type}"
 
         if i < len(key) - 1:
-            temp = llm._encode(", ")
+            temp = llm.encode(", ")
             for elem in temp:
                 lst.append(elem)
                 tokens.append(elem)
         else:
-            temp = llm._decode(lst)
+            temp = llm.decode(lst)
             if temp[-1] == ",":
                 temp = temp[:-1]
-            lst = llm._encode(temp)
+            lst = llm.encode(temp)
 
-    output: str = llm._decode(lst)
+    output: str = llm.decode(lst)
     output += "}}"
 
     with open(output_file, "r") as f:
         file = ""
         for line in f:
             file += line
-
     if file == "":
         output = f"[{output}]"
         ls = json.loads(output)
