c3cdfd62d39c01bc0eb027e18004baf1c2a382b3
ylau-sim <ylau-sim@f6r7s10.paris.42.school>
2026-09-25 11:01:01 +0200

    asdf

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-from .retrieval import get_text, get_sources
-from .chunker import write_chunks
-from .llm_model import get_answer, Small_LLM_Model
-from .pydantic_classes import (MinimalSearchResults,
-                               StudentSearchResultsAndAnswer, MinimalAnswer,
-                               RagDataset, StudentSearchResults)
